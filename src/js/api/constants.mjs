0adb44db28cacf483d10370ad30156d8a5a475ce
ehteht487 <92257659+ehteht487@users.noreply.github.com>
2024-05-11 12:56:25 +0200

    Adding formlistener

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -1,4 +1,4 @@
-export const API_HOST_URL = "https://v2.api.noroff.dev/";
-export const API_BASE = "";
-export const API_AUCTION_BASE = "";
-export const API_AUCTION_URL = ""; 
+// export const API_HOST_URL = "https://v2.api.noroff.dev/";
+// export const API_BASE = "";
+// export const API_AUCTION_BASE = "";
+// export const API_AUCTION_URL = ""; 
---
+++ src/js/handlers/register.mjs
@@ -0,0 +1,29 @@
+const form = document.querySelector("#registerForm");
+form.addEventListener("submit", (event) => {event.preventDefault()
+
+const form = event.target;
+const formData = new FormData(form);
+const profile = Object.fromEntries(formData.entries())
+
+const avatar = {
+avatarUrl: profile.avatarUrl,
+altText: profile.altText
+};
+
+delete profile.avatarUrl;
+delete profile.altText;
+
+profile.avatar = avatar;
+
+const banner = {
+bannerUrl: profile.bannerUrl,
+bannerAltText: profile.bannerAltText
+};
+
+delete profile.bannerUrl;
+delete profile.bannerAltText;
+
+profile.banner = banner;
+
+console.log(profile)
+});
